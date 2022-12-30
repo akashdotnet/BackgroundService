557887ede6f5a9bce09c4e8625c85769fb93301b
Akash <akashguptadotnet@outlook.com>
2022-12-30 20:59:43 +0530

    First commit for example...

---
+++ MyBackgroundServiceDemo/MyBackgroundServiceManager.cs
@@ -0,0 +1,32 @@
+﻿namespace MyBackgroundServiceDemo;
+public class MyBackgroundServiceManager : BackgroundService
+{
+    private readonly ILogger<MyBackgroundServiceManager> _logger;
+    private readonly IServiceProvider _myScopedService;
+    public MyBackgroundServiceManager(IServiceProvider myScopedService,
+        ILogger<MyBackgroundServiceManager> logger)
+    {
+        _logger = logger;
+        _myScopedService = myScopedService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            using (var scope = _myScopedService.CreateScope())
+            {
+                _logger.LogInformation("ExecuteAsync Started {dateTime}", DateTime.Now);
+                 var scopedService = scope.ServiceProvider.GetRequiredService<IScopedService>();
+                scopedService.Write();
+                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+            }
+        }
+    }
+
+    public override Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("StopAsync Started {dateTime}", DateTime.Now);
+        return base.StopAsync(stoppingToken);
+    }
+}
---
+++ MyBackgroundServiceDemo/MyScopedService.cs
@@ -0,0 +1,23 @@
+﻿namespace MyBackgroundServiceDemo;
+
+public class MyScopedService : IScopedService
+{
+    private readonly ILogger<MyScopedService> _logger;
+
+    public Guid Id { get; set; }
+
+    public MyScopedService(ILogger<MyScopedService> logger)
+    {
+        _logger = logger;
+        Id = Guid.NewGuid();
+    }
+    public void Write()
+    {
+        _logger.LogInformation("MyScopedService {Id}", Id);
+    }
+}
+
+public interface IScopedService
+{
+    void Write();
+}
